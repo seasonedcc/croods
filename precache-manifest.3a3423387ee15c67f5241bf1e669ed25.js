b716aa78ff0fed247ffaba484b0bc70295ded1c9
Gustavo Guichard <gustavoguichard@gmail.com>
2019-04-15 14:14:52 -0300

    Updates

---
+++ precache-manifest.3a3423387ee15c67f5241bf1e669ed25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "22d97dac7be5728782e3",
+    "url": "/croods-light/static/css/main.6ad69d63.chunk.css"
+  },
+  {
+    "revision": "22d97dac7be5728782e3",
+    "url": "/croods-light/static/js/main.ee5c2778.chunk.js"
+  },
+  {
+    "revision": "81f593f9c5386eda0991",
+    "url": "/croods-light/static/js/runtime~main.50297d70.js"
+  },
+  {
+    "revision": "39c6dc9169dac1dd3f91",
+    "url": "/croods-light/static/js/2.562ebbdd.chunk.js"
+  },
+  {
+    "revision": "df54975f14559d0822154522a8f0baf9",
+    "url": "/croods-light/index.html"
+  }
+];
