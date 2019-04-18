78ffbd9f56e64d746cf5c591d6223e2566af9233
Gustavo Guichard <gustavoguichard@gmail.com>
2019-04-17 21:11:11 -0300

    Hardcoded env var

--- example/src/basePath.js
+++ example/src/basePath.js
@@ -1,7 +1,1 @@
-const isDev = process.env.NODE_ENV === 'development'
-const SUB_DIRECTORY =
-  process.env.REACT_APP_SUB_DIRECTORY === undefined
-    ? '/croods-light'
-    : process.env.REACT_APP_SUB_DIRECTORY
-
-export default (isDev ? '' : SUB_DIRECTORY)
+export default ''
