8e32dbfcee298094e2fb30e6f5aeede0b18fbb74
Gustavo Guichard <gustavoguichard@gmail.com>
2019-04-17 21:06:52 -0300

    Add env var for deploy directory

--- example/src/basePath.js
+++ example/src/basePath.js
@@ -1,3 +1,7 @@
 const isDev = process.env.NODE_ENV === 'development'
+const SUB_DIRECTORY =
+  process.env.SUB_DIRECTORY === undefined
+    ? '/croods-light'
+    : process.env.SUB_DIRECTORY
 
-export default (isDev ? '' : '/croods-light')
+export default (isDev ? '' : SUB_DIRECTORY)
