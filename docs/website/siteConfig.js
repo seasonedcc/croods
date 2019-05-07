612c42759e168697d994e5dc2cfc3ccbfe2f3c71
= <v@fractopia.org>
2019-05-06 22:24:50 -0300

    Change name

--- docs/website/siteConfig.js
+++ docs/website/siteConfig.js
@@ -23,14 +23,14 @@ const users = [
 const siteConfig = {
   title: 'CROODS', // Title for your website.
   tagline: 'Yes.',
-  url: 'https://croods-light-docs.netlify.com', // Your website URL
+  url: 'https://croods.netlify.com', // Your website URL
   baseUrl: '/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: 'croods-docs',
+  projectName: 'croods',
   organizationName: 'Seasoned',
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
