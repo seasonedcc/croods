91cad8cde997db6c40e859476515b4fd37ce7a14
Gustavo Guichard <gustavoguichard@gmail.com>
2019-06-05 18:44:20 -0300

    Update rollup config to include regeneratorRuntime

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,8 @@
 import babel from 'rollup-plugin-babel'
 import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
-import postcss from 'rollup-plugin-postcss'
 import resolve from 'rollup-plugin-node-resolve'
 import url from 'rollup-plugin-url'
-import svgr from '@svgr/rollup'
 import json from 'rollup-plugin-json'
 
 import pkg from './package.json'
@@ -16,30 +14,22 @@ export default {
       file: pkg.main,
       format: 'cjs',
       sourcemap: true,
+      exports: 'named',
     },
     {
       file: pkg.module,
       format: 'es',
       sourcemap: true,
+      exports: 'named',
     },
   ],
+  external: ['react', 'react-dom'],
   plugins: [
     external(),
-    postcss({
-      modules: true,
-    }),
     url(),
-    svgr(),
-    babel({
-      exclude: 'node_modules/**',
-      plugins: ['external-helpers', '@babel/plugin-transform-regenerator'],
-    }),
-    resolve({
-      jsnext: true,
-      preferBuiltins: true,
-      browser: true,
-    }),
+    babel({ runtimeHelpers: true, exclude: 'node_modules/**' }),
+    resolve({ preferBuiltins: true, browser: true }),
     json(),
-    commonjs(),
+    commonjs({ include: 'node_modules/**' }),
   ],
 }
