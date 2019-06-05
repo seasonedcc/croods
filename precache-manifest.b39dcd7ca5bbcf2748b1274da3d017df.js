37a3d934c0b4ee54911afc99e88f9fe48cfa1f7a
Gustavo Guichard <gustavoguichard@gmail.com>
2019-06-05 19:48:11 -0300

    Updates

---
+++ precache-manifest.b39dcd7ca5bbcf2748b1274da3d017df.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fa756415279d5b28e337bea871f1f92",
+    "url": "/croods/index.html"
+  },
+  {
+    "revision": "c3ad7b71364bb2c4b93f",
+    "url": "/croods/static/css/main.3717e092.chunk.css"
+  },
+  {
+    "revision": "9023a1f40bda4b7ae9d1",
+    "url": "/croods/static/js/2.9b3a1823.chunk.js"
+  },
+  {
+    "revision": "c3ad7b71364bb2c4b93f",
+    "url": "/croods/static/js/main.9dc81ea0.chunk.js"
+  },
+  {
+    "revision": "1a0256a226adc82bccad",
+    "url": "/croods/static/js/runtime~main.4afd795c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/croods/precache-manifest.6c95bdaf3000ccd369f772e4d78f7caa.js"
+  "/croods/precache-manifest.b39dcd7ca5bbcf2748b1274da3d017df.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/croods/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/croods/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
