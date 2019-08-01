220eeffa7dab0389b753c059b6bd47958fffd850
Giovani <sousa_giovani@hotmail.com>
2019-08-01 15:00:08 -0300

    Fix buildUrl tests

--- src/__tests__/buildUrl.test.js
+++ src/__tests__/buildUrl.test.js
@@ -23,7 +23,7 @@ describe('buildUrl', () => {
   })
 
   it('does not append the ID to the end of customPath', () => {
-    const result = buildUrl({ customPath: 'fizz/buzz' })(10)
+    const result = buildUrl({ name: 'foo', customPath: 'fizz/buzz' })(10)
     expect(result).toBe('fizz/buzz')
   })
 
@@ -33,25 +33,30 @@ describe('buildUrl', () => {
   })
 
   it('uses customPath over path', () => {
-    const result = buildUrl({ path: 'foo/bar', customPath: 'fizz/buzz' })()
+    const result = buildUrl({ name: 'foo', path: 'foo/bar', customPath: 'fizz/buzz' })()
     expect(result).toBe('fizz/buzz')
   })
 
   it('removes extra slashes', () => {
-    const result = buildUrl({ path: '/foo////bar//' })()
+    const result = buildUrl({ name: 'foo', path: '/foo////bar//' })()
     expect(result).toBe('/foo/bar/')
   })
 
   it('replaces :id', () => {
-    const result = buildUrl({ customPath: 'foo/:id/bar' })(10)
+    const result = buildUrl({ name: 'foo', customPath: 'foo/:id/bar' })(10)
     expect(result).toBe('foo/10/bar')
 
-    const result2 = buildUrl({ path: 'foo/:id/bar' })(10)
+    const result2 = buildUrl({ name: 'foo', path: 'foo/:id/bar' })(10)
     expect(result2).toBe('foo/10/bar/10')
   })
 
   it('does not replace :id if id was not given', () => {
-    const result = buildUrl({ customPath: 'foo/:id/bar' })()
+    const result = buildUrl({ name: 'foo', customPath: 'foo/:id/bar' })()
     expect(result).toBe('foo/:id/bar')
+  })
+
+  it('returns / if name was not given', () => {
+    const result = buildUrl({ customPath: 'foo/:id/bar' })()
+    expect(result).toBe('/')
   })
 })
