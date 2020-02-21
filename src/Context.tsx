ab90af2f470cbef0b3b54f15028ce778297111d5
Giovani <sousa_giovani@hotmail.com>
2020-02-21 10:57:20 -0300

    Fix Context prop types

--- src/Context.tsx
+++ src/Context.tsx
@@ -15,7 +15,7 @@ CProvider.propTypes = {
   // the base API url for all requests
   // @ts-ignore
   baseUrl: CroodsPropTypes.url.isRequired,
-  children: PropTypes.elementType.isRequired,
+  children: PropTypes.element.isRequired,
   credentials: PropTypes.object,
   cache: PropTypes.bool,
   debugActions: PropTypes.bool,
